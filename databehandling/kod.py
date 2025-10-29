1cd928292c75b50bbe4dca220239ccdd8f20b4c0
zebastian <zebastian.andersson@iths.se>
2025-10-29 14:21:11 +0100

    databehandling

---
+++ databehandling/kod.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# _______________________________________________________________________________________dataframe from scratch
+
+# data = {"Kommun": ["Malmö", "Stockholm", "Uppsala", "Göteborg"],
+#         "Population": [347949, 975551, 233839, 583056]}
+# df = pd.DataFrame(data)
+
+
+# pop = 10379295 #totala mängden människor 
+
+# df_gbg = df["Kommun"] == "Göteborg" # min "mask" för att få sant eller falsk för att avgöra vad som ska printas ut 
+# top3 = df.sort_values("Population", ascending=False) #mask för att sortera efter värde
+
+# top3["population (%)"] = (top3["Population"]/pop)*100 # kalkulerar ut % och tar gånger 100 för att få det i heltal istället för att 1 ska va 100% 
+
+
+# print(df["Kommun"]) # A). printar ut hela colonen 
+# print()
+# print(df[df_gbg]) # B). kör en "mask" som jag kollar efter
+# print()
+# print(df.sort_values("Population", ascending=False)) # C). sortera med sort_values sen välja jag inom vilken colon man ska sorta efter och i detta fallet sorterar vi efter Population
+# print()
+# print(top3.head(3))# D). (head) för att bara få just den mängden
+# print()
+# print(top3.round(1))# E). (round) för att avrunda till 1 decimal 
+
+
+
+#____________________________cities in sweden - real dataset______________________
+
+
+
+
+path = r"C:\Users\stegi\Documents\github saker\python-programming-ZEBASTIAN-ANDERSSON\databehandling\komtopp50_2020.xlsx"
+
+df = pd.read_excel(path, sheet_name="Totalt", header=6, usecols="A:F")
+df.columns = ["Rang 2020",        "Rang 2019",    "Kommun", "Folkmängd 2020",	"Folkmängd 2019",	"Förändring"]
+print(df.head())
+df.info()
+# print("\n")
+# # print("\n")
+# print(df.sort_values("Rang 2020", ascending=False).head(5))
+
+# print("\n")
+# print(df["Folkmängd 2020"].sum())
+# print(df["Folkmängd 2019"].sum())
+
+
+
+fig, axes = plt.subplots(1, 2, figsize=(12,5))
+
+top5 = df.sort_values("Rang 2020").head(5)
+low5 = df.sort_values("Rang 2020").tail(5)
+
+
+axes[0].bar(top5["Kommun"],top5["Folkmängd 2020"])
+axes[1].bar(low5["Kommun"], low5["Folkmängd 2020"])
+
+plt.show()
--- labs/labb2/labb2.py
+++ labs/labb2/labb2.py
@@ -1,85 +1,96 @@
 import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
-
+import random as rnd
 #kollar avstånd 
 def kalkulator(test_data,känd_data):
     return np.sqrt(((känd_data[0]-test_data[0])**2)+((känd_data[1]-test_data[1])**2))
-
+     
 # separerar datan
 def sortera_data(datan):
     nya = np.array(datan)
     for data_points in nya:
+        posistion = tuple(data_points[0:2])
         if 0 in data_points[2:3]:
-            pichu.append(data_points[0:2])
+            pichu.append(posistion)
         else:
-            pikachu.append(data_points[0:2])
+            pikachu.append(posistion)
 
 # tar in dom kända data punkterna
-data = pd.read_csv(r"C:\Users\frolu\OneDrive\Skrivbord\skol kodning\labb2\datapoints.txt")
+data = pd.read_csv(r"C:\Users\stegi\Documents\github saker\python-programming-ZEBASTIAN-ANDERSSON\labs\labb2\datapoints2.txt")
 
-# Error hanterare / här vi skriver in input   
-while True:
-    try:          
-        x = 27 #x = float(input("skriv in bredd: "))
-        y = 32 #y = float(input("skriv in höjd: "))
-        if 0 > x or y < 0:
-            print("du kan inte mata in negativ nummer så försök igen och tänk på att det behöver vara positivt")
-        elif 0 == x or y == 0:
-            print("har svårt att tro att den va 0cm så kontrollera nummrena och skriv om resultatet")
-        else:
-            plt.text(x-0.5,y+0.5,"din punkt", size=8, weight="bold")
-            break
-    except ValueError:
-        print("text eller multiplikation av olika slag tillåts inte, va vänligen och skriv in ett positivt tal")
-
-# samling och sortering av test,pichu och pikachu data
-test_data = [(25, 32),(24.2, 31.5),(22, 34),(20.5, 34), (x,y)]
+# här skapar vi listor sen shufflar och delar upp dom
 pichu = []
 pikachu = []
+
+accuracys = []
 sortera_data(data)
 
-for xTest,yTest in test_data:
-    grannar = []
-    for i in pichu:
-        grannar.append(((kalkulator((xTest,yTest), i)),0))
-    for i in pikachu:
-        grannar.append(((kalkulator((xTest,yTest), i)), 1))
+for u in range(10):
+    # sufflar listorna och direkt efter delar jag upp listorna i träningsdata/test
+    rnd.shuffle(pichu)
+    rnd.shuffle(pikachu)
+    
+    träning_pikachu, test_pikachu = pikachu[:50], pikachu[50:]
+    träning_pichu, test_pichu =  pichu[:50], pichu[50:]
+    
+    rätt = 0
+    fel = 0
 
-    # lista samt distans som en backup om dom är likanära för att få bästa resultatet 
-    mängd_pikachu= []
-    mängd_pichu = []
-    distna_pikachu = 0
-    distan_pishu = 0 
+    for testpunkt in test_pichu+test_pikachu:
+        grannar = []
+        # här skapar vi ett facit 
+        if testpunkt in test_pikachu:
+            facit = 1
+        else: 
+            facit = 0
+        
+        for i in träning_pichu:
+            grannar.append((kalkulator(testpunkt , i), 0))
+        for i in träning_pikachu:
+            grannar.append((kalkulator(testpunkt , i), 1))
 
-    grannar.sort(key=lambda x: x[0])
-    for i,granne in grannar[0:10]:
-        if granne == 1:
-            mängd_pikachu.append(granne)
+        mängd_pikachu= []
+        mängd_pichu = []
+        distna_pika = 0
+        distan_pishu = 0 
+        
+        grannar.sort(key=lambda x: x[0])
+
+        for längd,granne in grannar[0:10]:
+            if granne == 1:
+                mängd_pikachu.append(granne)
+                distna_pika += längd
+            else:
+                mängd_pichu.append(granne)
+                distan_pishu += längd
+
+        # óm det är lika många så kollar vi distansen istället
+        if len(mängd_pikachu) == len(mängd_pichu): 
+            if distna_pika < distan_pishu:
+                gissning = 1
+            else:
+                gissning = 0
+   
+        # här drar vi vår gissning
         else:
-            mängd_pichu.append(granne)
+            if len(mängd_pikachu) > len(mängd_pichu):
+                gissning = 1
+            else:
+                gissning = 0
 
-    # kollar vem som är närmast och avgör vilken titel den får
-    if len(mängd_pikachu) == len(mängd_pichu):
-        if distna_pikachu < distan_pishu:
-           print(f"det blev en pikachu")
-           plt.scatter(xTest,yTest,c="red", edgecolors="black", marker="X", s=95)
+        # kontrollerar svaret
+        if gissning == facit:
+            rätt += 1
         else:
-            print(f"det blev en pichu")
-            plt.scatter(xTest,yTest,c="blue", edgecolors="black", marker="X",s=95) 
-            
-    elif len(mängd_pikachu) > len(mängd_pichu):
-        print(f"det blev en pikachu")
-        plt.scatter(xTest,yTest,c="red", edgecolors="black", marker="X", s=95)
-    else:
-        print(f"det blev en pichu")
-        plt.scatter(xTest,yTest,c="blue", edgecolors="black", marker="X",s=95)
+            fel += 1
+    #rättar resultatet
+    acc = rätt / (rätt + fel)
+    accuracys.append(acc)
 
-# skapar kordinaterna och plottar
-x,y=zip(*pichu)
-x1,y1=zip(*pikachu)
-
-plt.scatter(x,y,c="blue", edgecolors="black")
-plt.scatter(x1,y1,c="red", edgecolors="black")
-plt.title("röd = pikachu            blå = pichu            x = testpunkter")
-plt.show() 
+print(f"testet resulterade i {np.mean(accuracys):.2f}")
+plt.ylim(0,1)
+plt.xlim(0,10)
+plt.plot(accuracys)
+plt.grid()
+plt.show()
