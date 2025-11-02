f142c8e34fcb0169064918bdbcd677044f2bbe54
zebastian <zebastian.andersson@iths.se>
2025-11-02 11:23:26 +0100

    allt

--- databehandling/lab/labb.py
+++ databehandling/lab/labb.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 path = r"C:\Users\stegi\Documents\github saker\python-programming-ZEBASTIAN-ANDERSSON\databehandling\lab\College.csv"
 
-college = pd.read_csv(path, index_col=0)
+college = pd.read_csv(path)
 
 college = college.rename({'Unnamed: 0': 'College'}, axis=1)
-college = college.set_index('College')
+#college = college.set_index('College')
 
-print(college.info())
+print(college)
 # print("\n\n\n")
+
